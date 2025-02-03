9e25253a51eab3908245b8e9d099e7665cd98dfc
Teja-Srinivas509 <cheedallatejasrinivas@gmail.com>
2025-02-03 21:57:53 +0530

    fourth commit

---
+++ docs/browser/server/assets-chunks/index_csr_html.mjs
@@ -0,0 +1,14 @@
+export default `<!doctype html>
+<html lang="en" data-beasties-container="">
+<head>
+  <meta charset="utf-8">
+  <title>Rookie</title>
+  <base href="/">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="icon" type="image/x-icon" href="favicon.ico">
+<link rel="stylesheet" href="styles-5INURTSO.css"></head>
+<body ngcm="">
+  <app-root></app-root>
+<link rel="modulepreload" href="chunk-A3Z4CKF2.js"><link rel="modulepreload" href="chunk-H7GECWOW.js"><script src="polyfills-FFHMD2TL.js" type="module"></script><script src="scripts-EEEIPNC3.js" defer=""></script><script src="main-TBPEKFVE.js" type="module"></script></body>
+</html>
+`;
