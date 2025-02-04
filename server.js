70177b003692d33e05b151ec1a1ac878b763b5c8
Teja-Srinivas509 <cheedallatejasrinivas@gmail.com>
2025-02-04 10:30:15 +0530

    Fixed build and server configuration

--- server.js
+++ server.js
@@ -2,12 +2,13 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-app.use(express.static(__dirname + '/dist/rookie'));
+app.use(express.static(path.join(__dirname, 'dist/rookie')));
 
 app.get('/*', (req, res) => {
-  res.sendFile(path.join(__dirname + '/dist/rookie/index.html'));
+  res.sendFile(path.join(__dirname, 'dist/rookie/index.html'));
 });
 
-app.listen(process.env.PORT || 8080, () => {
-  console.log('App running on port', process.env.PORT || 8080);
+const port = process.env.PORT || 8080;
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
 });
